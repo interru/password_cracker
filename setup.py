b0828e00a42a04397cf7447d756d9081a16e06a6
Mathias Koehler <mail@mathias.im>
2014-12-14 14:01:12 +0100

    Demo OpenCL application

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+password_cracker
+~~~~~~~~~~~~~~~~
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='password_cracker',
+    version='0.1-dev',
+    packages=['cracker'],
+    author='Password_Cracker Team',
+    author_email='mail@mathias.im',
+    url='http://github.com/interru/password_cracker',
+    description='A simple opencl sha256 password cracker',
+    keywords='Password Hash Cracker',
+    long_description=__doc__,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
+)
